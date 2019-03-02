9da60c7c08d86ddf5633d98d068db003feae2289
Colin Campbell <colin@MacBook-Pro.local>
2019-03-02 13:44:22 -0500

    Fix bash profile output

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ mv -vn ~/.vimrc ~/.vimrc.bk
 mv -vn ~/.tmux.conf ~/.tmux.conf.bk
 mv -vn ~/.gitconfig ~/.gitconfig.bk
 
-printf "source '$DOTFILE_HOME/bash_profile" > ~/.bash_profile
+printf "source '$DOTFILE_HOME/bash_profile'" > ~/.bash_profile
 printf "so $DOTFILE_HOME/vimrc.vim" > ~/.vimrc
 printf "source-file $DOTFILE_HOME/tmux.conf" > ~/.tmux.conf
 printf "[include]\n\tpath = $DOTFILE_HOME/gitconfig" > ~/.gitconfig 
