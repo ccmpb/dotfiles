3422ce6e93e8ddf5b24ec549ada4ef9118138d83
Colin Campbell <colin@elusivelabs.com>
2021-12-23 02:17:22 -0500

    Add some os specific smarts.

--- bin/freespace.sh
+++ bin/freespace.sh
@@ -5,15 +5,18 @@ set -e -o pipefail
 figlet "free your space..."
 
 # docker
-docker system prune -f
+sudo docker system prune -af
 
 # homebrew
-brew cleanup -s
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    brew cleanup -s
+fi
 
 # old dev dependencies
 find ~/devel -name "venv|env" -type d -mtime +120 | xargs rm -rf
 find ~/devel -name "node_modules" -type d -mtime +120 | xargs rm -rf
 
 
-
-diskutil info / | grep "Free Space"
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    diskutil info / | grep "Free Space"
+fi
